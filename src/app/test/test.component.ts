d52ba7f212f1f14df08d65f24864995f205766df
SheylaAlejos <sheyla.alejos@tecsup.edu.pe>
2018-04-29 20:30:38 -0500

    diseño

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: '<div>incline </div>',
+  styles: ['div{color: red;}']
+
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
